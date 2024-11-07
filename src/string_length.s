760ae2af1b8a20e4bc701f9f497e446b2710dcfa
James Graves <james.c.graves.jr@gmail.com>
2024-11-07 16:46:56 +0800

    a1 was already being updated with vl
    The vsetvli already specifies that a1 will be updated
    with vl during each iteration, so it isn't necessary
    to read the chip status register (CSR) to get this.

--- src/string_length.s
+++ src/string_length.s
@@ -5,7 +5,6 @@ strlen:
 loop:
 	vsetvli     a1, x0, e8, m8, ta, ma   # Vector of bytes of maximum length
 	vle8ff.v    v8, (a3)                 # Load bytes
-	csrr        a1, vl                   # Get bytes read
 	vmseq.vi    v0, v8, 0                # Set v0[i] where v8[i] = 0
 	vfirst.m    a2, v0                   # Find first set bit
 	add         a3, a3, a1               # Bump pointer
